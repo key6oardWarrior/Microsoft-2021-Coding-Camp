db8373e72efb7dcb27a80eb873765af7725f3779
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2021-06-22 20:28:20 -0400

    Added day 5 lesson

---
+++ Labs/Day 5/Lab 1/README.md
@@ -0,0 +1,47 @@
+<!DOCTYPEhtml>
+<html lang="en-US">
+ <body>
+
+<h1>Strings and List</h1>
+
+<h2>Pratice</h2>
+
+<ol>
+	<li>Print a string literal</li>
+	<li>Create a var that holds a string</li>
+	<li>Splice the string var that you created</li>
+	<li>Fix string code</li>
+</ol>
+
+<h2>Cut These Strings Up!</h2>
+
+<p>Rules:</p>
+<ol>
+	<li>Have a way for users to enter their name</li>
+	<li>Print the number of characters that the string has</li>
+	<li>Characters in position 0, 3, and 6 must be printed. If a string does not have a character in a given position the don't print it</li>
+</ol>
+
+<h2>Find Your Name:</h2>
+
+<p>Rules:</p>
+<ol>
+	<li>You have a list of capital and lower case letters</li>
+	<li>Use a loop, either for, for-each, or while, to find the letters in your name.</li>
+	<li>Print your name on <b>ONE</b> line</p>
+</ol>
+
+<hr />
+<h2>Extra Lesson</h2>
+<hr />
+<h2>Print the names in the file</h2>
+
+<p>Rules:</p>
+<ol>
+	<li>Do <b>NOT</b> erase the bolier place code</li>
+	<li>Print the number of characters that the string has</li>
+	<li>Characters in position 0, 5, and -8 must be printed. If a string does not have a character in a given position the don't print it</li>
+</ol>
+
+ </body>
+</html>
---
+++ Labs/Day 5/Lab 1/String_Splicing.py
@@ -0,0 +1,50 @@
+USER_NAME = input("Enter your name: ")
+nameIndex = 6
+strLen = len(USER_NAME)
+print(f"This string has {strLen} characters.")
+
+if strLen > nameIndex:
+	print(f"Characters printed in positions 0, 3, and {nameIndex} are: {USER_NAME[0]} {USER_NAME[3]} {USER_NAME[nameIndex]}")
+elif strLen > 3:
+	print(f"Characters printed in positions 0, and 3 are: {USER_NAME[0]} {USER_NAME[3]}")
+elif strLen >= 1:
+	print(f"Character printed in positions 0 is: {USER_NAME[0]}")
+else:
+	print("String needs to be longer than 0")
+
+vals = (-5, -1)
+toBePrinted = f"The characters printed in positions -8, {vals[0]}, and {vals[1]} are: "
+
+# 2
+
+CAP = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+LOWER = 'abcdefghijklmnopqrstuvwxyz'
+
+name = ""
+for i in "Name":
+	if i != i.lower():
+		index = CAP.index(i)
+		name += CAP[slice(index, index+1)]
+	else:
+		index = LOWER.index(i)
+		name += LOWER[slice(index, index+1)]
+
+print(name)
+
+# 3
+import os # don't change
+
+PATH = os.path.dirname(__file__) # don't change
+nameIndex = -8
+for i in open(os.path.join(PATH, "info.txt"), "r").readlines(): # don't change
+	strLen = len(i)
+	print(i)
+	# code pass this point should not be given to students until they have finished this project
+	if strLen > abs(nameIndex):
+		print(f"Characters printed in positions 0, 3, and {nameIndex} are: {i[0]} {i[3]} {i[nameIndex]}")
+	elif strLen > 5:
+		print(f"Characters printed in positions 0, and 3 are: {i[0]} {i[3]}")
+	elif strLen >= 1:
+		print(f"Character printed in positions 0 is: {i[0]}")
+	else:
+		print("String needs to be longer than 0")
