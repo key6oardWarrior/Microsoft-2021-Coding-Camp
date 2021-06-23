8fe2c8a80585b59e8127d9b17932835619f57414
Alexander W <36136088+SnoodTood@users.noreply.github.com>
2021-06-23 12:35:07 -0400

    Add files via upload

---
+++ Labs/Day 6/Lab 1/lab.py
@@ -0,0 +1,30 @@
+import random
+# 1: Dice Roller
+
+# This function has already been written
+# It rolls one die with an arbitrary number of sides
+# Use it when filling in roll_dice()
+def roll_one(sides):
+    return random.randint(1, sides)
+    
+# Complete this function as directed in the README
+def roll_dice():
+    total = 0
+    rolls = []
+    
+    num_dice = input("How many dice would you like to roll?\n> ")
+    num_sides = input("How many sides do those dice have?\n> ")
+    
+    # Write some code here...
+    
+
+# 2: Email Generator
+
+# Complete this function as directed in the README
+# We encourage defining additional functions in order to complete this
+def gen_email(first, last):
+    out = ""
+    
+    # Write some code here...
+    
+    return out
