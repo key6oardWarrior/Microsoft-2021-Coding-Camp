334739c37160213efba2cbadc28b242928eebc69
key6oardWarrior <42715109+key6oardWarrior@users.noreply.github.com>
2021-07-01 09:49:30 -0400

    Update learn_pygame.py

--- Labs/Day 7/Lab 1/learn_pygame.py
+++ Labs/Day 7/Lab 1/learn_pygame.py
@@ -1,39 +1,14 @@
-import os
 import pygame
-
-def display(FILE_PATH, POSITION):
-	'''
-	@param FILE_PATH - <class 'str'> File path of the png image to be displayed\n
-	@param POSITION - <class 'tuple'> the x and y position of the image\n
-	@return <class 'pygame.Surface'> the image that the user wants to display
-	'''
-	global window
-
-	image = pygame.image.load(FILE_PATH)
-	window.blit(image, POSITION)
-	
-	return image
-
-def updatePlayer():
-	global playerPos
-	KEY = pygame.key.get_pressed()
-	
-	if KEY[pygame.K_LEFT]:
-		playerPos[0] += 100
-	elif KEY[pygame.K_RIGHT]:
-		playerPos[1] += 100
 
 pygame.init()
 WIDTH = 1000
 HEIGHT = 1000
-BACKGROUND_COLOR = (255, 255, 255) # black
+BACKGROUND_COLOR = (0, 0, 0) # black
 FPS = 60 # frames per second
 clock = pygame.time.Clock()
 window = pygame.display.set_mode((WIDTH, HEIGHT))
+window.fill(BACKGROUND_COLOR)
 
-playerPos = [0, 0]
-# give the students this line of code
-player = display(f"{os.path.dirname(__file__)}\\assets\\player.png", tuple(playerPos))
 isRunning = True
 
 while isRunning:
@@ -45,12 +20,10 @@ while isRunning:
 			isRunning = False
 
 	# code for game play will go here
-	window.blit(player, tuple(playerPos))
+	window.blit(player, (playerPos[0], playerPos[1]))
 	updatePlayer()
 
 	# always the last lines of code to run to update the game screen
-	window.fill(BACKGROUND_COLOR)
 	pygame.display.flip()
 	clock.tick(FPS)
-	pygame.display.set_caption(f"FPS: {FPS}")
-	pygame.display.update()
+	pygame.display.set_caption(f"FPS: {clock.get_fps()}")
